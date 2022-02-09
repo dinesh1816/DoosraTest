8c7feb2f19514c907a80049a16bf9d9ec08a5890
Saddam Hussain <saddam.hussain@doosra.io>
2022-02-09 18:14:19 +0530

    Added Bitbucket CI CD sample configuration Added CodeDeploy configurations

---
+++ shells/install_dependencies.sh
@@ -0,0 +1,17 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PM2_HOME=/home/captain/.pm2
+
+echo "NPM Version"
+npm -v
+
+echo "PM2 Version"
+pm2 -v
+
+# Go to app folder
+cd /home/captain/deployments/sample-micro-service
+
+# Installing dependencies with npm
+npm install
---
+++ shells/stop_server.sh
@@ -0,0 +1,17 @@
+# #!/bin/bash
+# echo "Stopping Server"
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# export PM2_HOME=/home/captain/.pm2
+
+# echo "PM2 Version"
+# pm2 -v
+
+# # Go to app folder
+# cd /home/captain/deployments/sample-micro-service
+
+# # stop running process
+# # pm2 stop sample-micro-service
