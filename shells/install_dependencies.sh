62560668d959e70b307595cd64b3c3b971e7e431
Saddam Hussain <saddam.hussain@doosra.io>
2022-02-14 19:03:59 +0530

    Use node 16 for this service deployment

--- shells/install_dependencies.sh
+++ shells/install_dependencies.sh
@@ -2,6 +2,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Use node 16.x for running this app
+nvm use 16
+
 export PM2_HOME=/home/captain/.pm2
 
 echo "NPM Version"
