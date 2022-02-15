499b34efa39d21c0466c6ea42c693380aac99cd6
Saddam Hussain <saddam.hussain@doosra.io>
2022-02-15 12:34:54 +0530

    Connect using redis connection string Added additional redis client listeners Handle redis connect exception

--- connections/RedisClient.js
+++ connections/RedisClient.js
@@ -1,16 +1,26 @@
 import { createClient } from "redis";
-// import config from "../config/index";
+import config from "../config/index";
 
 // eslint-disable-next-line import/no-mutable-exports
 let RedisClient;
 
 (async () => {
-  RedisClient = createClient();
+  RedisClient = createClient({
+    url: config.redisConfig.connString,
+  });
 
+  RedisClient.on("connect", () => console.log("Redis Client Initiating Connection..."));
+  RedisClient.on("ready", () => console.log("Redis Client Connected Successfully."));
+  RedisClient.on("reconnecting", () => console.log("Redis Client Reconnecting..."));
+  RedisClient.on("end", () => console.log("Redis Client Disconnected."));
   RedisClient.on("error", (err) => console.log("Redis Client Error", err));
 
-  await RedisClient.connect();
-  console.log("Redis Client Connected", RedisClient);
+  try {
+    await RedisClient.connect();
+    console.log("Redis Client Connected", RedisClient);
+  } catch (err) {
+    console.error("Redis Client Exception", err);
+  }
 })();
 
 export default RedisClient;
